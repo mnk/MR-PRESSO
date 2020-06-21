20d98b7a273051885a652904628d49f364b84a7d
Mathias Nicolajsen Kjaergaard <mnk@mnk.dk>
2020-06-21 21:35:58 +0200

    Calculate NbDistribution from number of rows in input file

--- main.R
+++ main.R
@@ -7,7 +7,7 @@ file.names <- dir(path="data", pattern="*.csv", full.names=TRUE)
 for(i in 1:length(file.names)){
   print(file.names[i])
   inputFile  = as.data.frame(read_csv(file.names[i]))
-  result <- lib$mr_presso$mr_presso(BetaOutcome = "betaoutcome", BetaExposure = "betaexposure", SdOutcome = "seoutcome", SdExposure = "seexposure", OUTLIERtest = TRUE, DISTORTIONtest = TRUE, data = inputFile, NbDistribution = 10000,  SignifThreshold = 0.05)
+  result <- lib$mr_presso$mr_presso(BetaOutcome = "betaoutcome", BetaExposure = "betaexposure", SdOutcome = "seoutcome", SdExposure = "seexposure", OUTLIERtest = TRUE, DISTORTIONtest = TRUE, data = inputFile, NbDistribution = nrow(inputFile)*50,  SignifThreshold = 0.05)
   row.names(result) <- str_replace(file.names[i], ".*?_(.*)\\.csv", "\\1")
   if (exists("out.file")) {
     out.file <- rbind(out.file, result)
